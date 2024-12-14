d06428ada59661d16762bf0b9878f25272b31a05
Danno Ferrin <danno@numisight.com>
2024-12-14 10:36:21 -0700

    Add trophy
    Add geth trophy.  Also, trophies now should only be acknowledged when an
    associated test PR is posted.
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>

--- src/ethereum_fuzzer_differential/__init__.py
+++ src/ethereum_fuzzer_differential/__init__.py
@@ -9,8 +9,8 @@ The following bugs were found using the differential fuzzer
 
 1. Besu EOFCREATE memory sizes clamped to i32 instead of i64, impacting gas calculations
    (found with ReplacePushWithAddress).
-   Besu [PR #7979](https://github.com/hyperledger/besu/pull/7979)
+   Test [PR #989](https://github.com/ethereum/execution-spec-tests/pull/989)
 2. Geth DATACOPY values could overflow uint64 (found with ReplacePushWithMagic)
-   [Commit](https://github.com/shemnon/go-ethereum/commit/5ce932e592511d491ea535f07989f63e50dd35d5)
+   Test [PR #1020](https://github.com/ethereum/execution-spec-tests/pull/1020)
 
 """
