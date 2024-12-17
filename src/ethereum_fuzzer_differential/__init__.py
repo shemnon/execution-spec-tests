4087f18ded22a207c3c6cd250fbb46c0d1f731d8
Danno Ferrin <danno@numisight.com>
2024-12-17 16:13:48 -0700

    trophy update
    Signed-off-by: Danno Ferrin <danno@numisight.com>

--- src/ethereum_fuzzer_differential/__init__.py
+++ src/ethereum_fuzzer_differential/__init__.py
@@ -14,5 +14,11 @@ The following bugs were found using the differential fuzzer
    Test [PR #1020](https://github.com/ethereum/execution-spec-tests/pull/1020)
 3. Nethermind had RIP-7212 p256verify activated in Prague.
    Test [PR #1021](https://github.com/ethereum/execution-spec-tests/pull/1021)
+4. Besu created a "ghost" account when calling a non-existent account that prevented a subsequent
+   value-bearing EXTCALL from being charged for account creation.
+   Test [PR #1025](https://github.com/ethereum/execution-spec-tests/pull/1025)
+5. Evmone does not have a fully functional expmod precompile. Clients under test need to be
+   configured to use [an
+   external library.](https://github.com/ethereum/evmone/blob/master/README.md?plain=1#L100-L102)
 
 """
